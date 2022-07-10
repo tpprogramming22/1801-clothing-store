40a9031d4b1af935429b4f2bfa54d39af7304444
ted <tpprogrammingcontact@gmail.com>
2022-07-10 20:41:53 +0100

    apple pay complete v3

--- src/components/apple-pay.jsx
+++ src/components/apple-pay.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
-import { useStripe, useElements, PaymentRequestButtonElement } from "@stripe/react-stripe-js";
+import {
+  useStripe,
+  useElements,
+  PaymentRequestButtonElement,
+} from "@stripe/react-stripe-js";
 
 const ApplePay = () => {
   const stripe = useStripe();
@@ -22,16 +26,51 @@ const ApplePay = () => {
       },
     });
     pr.canMakePayment().then((result) => {
-        if(result){
-            setPaymentRequest(pr)
+      if (result) {
+        setPaymentRequest(pr);
+      }
+    });
+
+    pr.on("paymentmethod", async (e) => {
+      const response = await fetch(
+        "/.netlify/functions/create-payment-intent",
+        {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            paymentMethodType: "card",
+          }),
         }
-    })
+      ).then((res) => res.json());
+
+      const {
+        paymentIntent: { client_secret },
+      } = response;
+
+      const { paymentIntent } = await stripe.confirmCardPayment(
+        client_secret,
+        {
+          payment_method: e.paymentMethod.id,
+        },
+        {
+          handleActions: false,
+        }
+      )
+      e.complete('success')
+      if(paymentIntent.status == 'requires_action') {
+        stripe.confirmCardPayment(client_secret);
+      }
+    });
   }, [stripe, elements]);
 
   return (
     <div>
       <h1 className="text-center text-4xl">Apple Pay Header</h1>
-      {paymentRequest && <PaymentRequestButtonElement options={{paymentRequest}}/>}
+      {paymentRequest && (
+        <PaymentRequestButtonElement options={{ paymentRequest }} />
+      )}
     </div>
   );
 };
