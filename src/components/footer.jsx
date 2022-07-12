d4ec1ee59ce085544432e088207b9c78add25f73
ted <tpprogrammingcontact@gmail.com>
2022-07-12 17:00:24 +0100

    fixed footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,13 @@
+import {AiTwotoneMail} from 'react-icons/ai'
 const Footer = () => {
   return (
     <div>
       <footer className="bg-gray-300 w-[400px]">
-        <h1>Contact us for any queries!</h1>
-        <a href='mailto: 1801helpdesk@gmail.com' className="text-xs text-white">Send email</a>
+        <h1 className='pt-2'>Contact us for any queries!</h1>
+        <div className="flex justify-evenly items-center">
+       <a href='mailto: 1801helpdesk@gmail.com' className='text-xl pb-3'><AiTwotoneMail /></a>
+        </div>
+        
       </footer>
     </div>
   );
