5ce519934d023ad7a7d82f97c5d8f7d84837acf0
ted <tpprogrammingcontact@gmail.com>
2022-08-16 22:08:10 +0100

    testing v9

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import {AiTwotoneMail} from 'react-icons/ai'
 const Footer = () => {
   return (
     <div>
-      <footer className="bg-gray-300 w-[400px]">
+      <footer className="bg-gray-300 w-[450px]">
         <h1 className='pt-2'>Contact us for any queries!</h1>
         <div className="flex justify-evenly items-center">
        <a href='mailto: cclubhelp@gmail.com' className='text-xl pb-3'><AiTwotoneMail /></a>
