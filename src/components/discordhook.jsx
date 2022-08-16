e370743bbcc137c336a233c5c07b2c30fee178bb
ted <tpprogrammingcontact@gmail.com>
2022-08-16 21:29:21 +0100

    testing v5

--- src/components/discordhook.jsx
+++ src/components/discordhook.jsx
@@ -1,7 +1,7 @@
-function DiscordHook() {
+function DiscordHook(intent) {
     const botUrl = 'https://discord.com/api/webhooks/1009155207361466508/-cl2sZ7TGrIbbxZdHKyLc_GZNcJsJSQq7J3sf9hA1x-mjva1DRfvlKT0T9fKtGtswY18'
     const botMessage = {
-        "content": `Order received!`
+        "content": `Order received! ${intent}`
     }
     fetch(botUrl,{
         "method":"POST", "headers":{"content-type":"application/json"},
